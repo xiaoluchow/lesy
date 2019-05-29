714e546157f449a4e91d20f89813434a7397be4c
xiaolu.zhou <xiaolu.zhou@yourhome.com.cn>
2019-05-29 19:02:43 +0800

    1111

---
+++ src/main/java/com/xiaolu/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.xiaolu.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping
+    public String getUser(@PathVariable int userId) {
+        return "";
+    }
+}
